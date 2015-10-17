e011939b2c9319096acc9e9240d549ff57481e63
dariothornhill <dariothornhill@gmail.com>
2015-10-17 19:23:52 -0400

    Refactored the app to use reouting and prettified all the things!

--- app.js
+++ app.js
@@ -1,61 +1,103 @@
-(function(){
-var app = angular.module('Store',['ngRoute']);
+(function() {
+    var app = angular.module('Store', ['ngRoute']);
 
-app.controller('StoreController', function(){
- this.products = items;
- this.showProducts = false;
- this.newProduct ={};
- this.detailedProduct = {};
- this.showDetailsFlag = false;
- this.showForm = false;
+    app.factory("products", function() {
+        this.products = {};
+        this.products.list = items; /*Get from http.get*/
+        this.products.add = function(product) {
+            this.list.push(product);
+        };
+        this.products.getAll = function() {
 
- this.addProduct = function(){
- 	this.products.push(this.newProduct);
- 	this.newProduct.id = 4; //there is some function to generate a new id
- 	this.showProducts();
- 	this.newProduct = {};
- };
- this.browseProducts = function(){
- 	this.detailedProduct = {}; 
- 	this.showProducts = true;
- 	this.showForm= false;
+            return this.list;
+        };
+        this.products.remove = function($id) {
+            var i = 0;
+            for (; i < this.products.list.length - 1; i++) {
+                if (this.products.list[i].id === id) {
+                    delete this.products.list[i].id;
+                }
+                break;
+            }
+        }
+        this.products.show = function($id) {
+            var i = 0;
+            for (; i < this.products.list.length - 1; i++) {
+                if (this.products.list[i].id === id) {
+                    return this.products.list[i].id;
+                }
+                break;
+            }
+        };
+        return this.products;
+    });
 
- };
- this.editProducts = function(){};
- this.removeProducts = function(){};
- this.showAddForm = function(){
- 	this.showForm= true;
- 	this.showProducts = false;
+    app.controller('AppController', function() {
+        this.products = items;
+    });
 
- }; 
+    app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+            when('/list', {
+                templateUrl: 'partials/list-products.html',
+                controller: 'ListProductsController'
+            }).
+            when('/new', {
+                templateUrl: 'partials/add-product.html',
+                controller: 'NewProductController'
+            }).when('/:id/edit', {
+                templateUrl: 'partials/edit-.html',
+                controller: 'EditProductController'
+            }).
+            when('/:id/show', {
+                templateUrl: 'partials/product-details.html',
+                controller: 'ProductDetailsController'
+            }).
+            otherwise({
+                redirectTo: '/list'
+            });
+        }
+    ]);
 
- this.showDetails = function(id){
- 	var i = 0;
- 	for(;i <this.products.length -1 ;i++){
- 		if(this.products[i].id === id){
- 			this.detailedProduct = this.products[i];
- 		}
- 		break;
- 		this.showProducts = false;
- 	    this.showForm= false;
+    app.controller('NewProductController', function(products) {
 
- 	}
+    });
 
+    app.controller("ProductDetailsController", function(products) {
 
- };
+    });
 
-});
+    app.controller("ListProductsController", function(products) {
+        var self = this;
+        self.products = products.getAll();
 
-app.directive("productDetails",function(){
-	return {
-		restrict:'E',
-		templateUrl:'product-details.html'
-	};
-});
+    });
 
-var items = [
-{id: 1, name: '1990 Toyota Corolla', category: 'car', price: 1500.00, photo: 'car1.jpg', description:"It's fast but has not AC"},
-{id: 2, name: '1995 Suzuki Swift', category: 'car', price: 7500.00, photo: 'car2.jpg', description:"It's has the AC but ain't so swift"},
-{id: 3, name: '2000 Toyota Corolla', category: 'car', price: 9500.00, photo: 'car3.jpg', description:"It's fast and the AC works"}
-]
+    app.controller("EditProductController", function(products) {
+
+    });
+
+    var items = [{
+        id: 1,
+        name: '1990 Toyota Corolla',
+        category: 'car',
+        price: 1500.00,
+        photo: 'car1.jpg',
+        description: "It's fast but has not AC"
+    }, {
+        id: 2,
+        name: '1995 Suzuki Swift',
+        category: 'car',
+        price: 7500.00,
+        photo: 'car2.jpg',
+        description: "It's has the AC but ain't so swift"
+    }, {
+        id: 3,
+        name: '2000 Toyota Corolla',
+        category: 'car',
+        price: 9500.00,
+        photo: 'car3.jpg',
+        description: "It's fast and the AC works"
+    }];
 })();
